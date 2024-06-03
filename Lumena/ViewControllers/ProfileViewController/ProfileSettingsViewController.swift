3d46e1f5ccafe1454f791edf99303f455bce68ab
hika583 <hshim023@ucr.edu>
2024-06-03 00:45:16 -0700

    toolbar button in profile fixed with delegate issues

--- Lumena/ViewControllers/ProfileViewController/ProfileSettingsViewController.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ProfileSettingsViewController.swift
-//  Lumena
-//
-//  Created by 島田晃 on 2024/05/30.
-//
-
-import Foundation
--- amplify/generated/models/TagTrackQL.swift
+++
@@ -1,8 +0,0 @@
-// swiftlint:disable all
-import Amplify
-import Foundation
-
-public struct TagTrackQL: Embeddable {
-  var trackID: String
-  var tagMusicRange: [Double]?
-}
