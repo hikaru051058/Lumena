6ac808799f087fe9a3ab5ad317bf9ba8807bfb3d
hikaru051058 <cystjp510@gmail.com>
2024-07-24 13:48:08 +0900

    Skin Settings updated & glitch in infinite scroll fixed

--- AmplifyModels/FollowStatus.swift
+++
@@ -1,9 +0,0 @@
-// swiftlint:disable all
-import Amplify
-import Foundation
-
-public enum FollowStatus: String, EnumPersistable {
-  case pending = "PENDING"
-  case approved = "APPROVED"
-  case rejected = "REJECTED"
-}
--- Pods/Target Support Files/Pods-Lumena/Pods-Lumena-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lumena : NSObject
-@end
-@implementation PodsDummy_Pods_Lumena
-@end
